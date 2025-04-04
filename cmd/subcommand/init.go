c0dd2ad597643feceecce59bdec073662cc99d2f
hippocampa <tsdhrm@outlook.com>
2025-04-04 17:40:36 +0800

    Move initialization logic to core package

--- cmd/subcommand/init.go
+++ cmd/subcommand/init.go
@@ -1,11 +1,7 @@
 package subcommand
 
 import (
-	"fmt"
-	"os"
-	"path/filepath"
-
-	"github.com/hippocampa/dotshadow/commons"
+	"github.com/hippocampa/dotshadow/core"
 	"github.com/urfave/cli/v2"
 )
 
@@ -13,25 +9,6 @@ func InitCmd() *cli.Command {
 	return &cli.Command{
 		Name:   "init",
 		Usage:  "Create dotfiles folder",
-		Action: InitAction,
+		Action: core.Init,
 	}
-}
-
-func InitAction(context *cli.Context) error {
-	dotfilesDir, err := commons.GetDotFilesDir()
-	if err != nil {
-		return err
-	}
-	// create .dotfiles in homedirectory
-	if err := os.Mkdir(dotfilesDir, 0o755); err != nil {
-		return err
-	}
-
-	// crate index.json
-	if _, err := os.Create(filepath.Join(dotfilesDir, "index.json")); err != nil {
-		return err
-	}
-	fmt.Printf("Intialized:\n.dotfiles\t%v\nindex.json\t%v\n", dotfilesDir, filepath.Join(dotfilesDir, "index.json"))
-
-	return nil
 }
