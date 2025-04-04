9da3e47f976042e10c892d6481f9395627e76a5d
hippocampa <tsdhrm@outlook.com>
2025-04-04 18:03:31 +0800

    Add Track command implementation and skeleton for Copy function

--- core/tracker.go
+++ core/tracker.go
@@ -1,1 +1,20 @@
 package core
+
+import (
+	"fmt"
+	"path/filepath"
+
+	"github.com/hippocampa/dotshadow/utils"
+	"github.com/urfave/cli/v2"
+)
+
+func Track(context *cli.Context) error {
+	source := context.Args().Get(0)
+	fmt.Println(filepath.Abs(source))
+	_, err := utils.GetDotfilesRoot()
+	if err != nil {
+		return fmt.Errorf("GetDotfilesRoot: %w", err)
+	}
+
+	return nil
+}
---
+++ storage/copy.go
@@ -0,0 +1,5 @@
+package storage
+
+func Copy(source string, destination string) error {
+	return nil
+}
