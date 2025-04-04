5e5f767b42fcdb76a75b220c75c7eb6150ed8e59
hippocampa <tsdhrm@outlook.com>
2025-04-04 17:42:30 +0800

    Add initializer for managing dotfiles configuration

---
+++ core/initializer.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/hippocampa/dotshadow/utils"
+	"github.com/urfave/cli/v2"
+)
+
+func Init(context *cli.Context) error {
+	dotfilesRoot, err := utils.GetDotfilesRoot()
+	if err != nil {
+		return fmt.Errorf("GetDotfilesRoot: %w", err)
+	}
+	// create .dotfiles in homedirectory
+	if err := os.Mkdir(dotfilesRoot, 0o755); err != nil {
+		return err
+	}
+
+	// crate index.json
+	if _, err := os.Create(filepath.Join(dotfilesRoot, "index.json")); err != nil {
+		return err
+	}
+	fmt.Printf("Intialized:\n.dotfiles\t%v\nindex.json\t%v\n",
+		dotfilesRoot, filepath.Join(dotfilesRoot, "index.json"))
+
+	return nil
+}
